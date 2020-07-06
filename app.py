3a76f5f1fb295c70268156ce20ae9fca76d206e5
alphadatagamma <alphadatagamma@users.noreply.github.com>
2020-07-06 18:34:35 -0400

    Editing source link

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def main():
         "This an interactive streamlit app completely created with Python's latest library **streamlit** "
         "Do reach out to me on [LinkedIn](https://www.linkedin.com/in/abhishek-gupta-/) or "
         "at [Mail me](mailto:abhishek.2.gupta@uconn.edu) to know more. "
-        "Also check the [source code](https://github.com/MarcSkovMadsen/awesome-streamlit) here. "  
+        "Also check the [source code](https://github.com/alphadatagamma/Streamlit-Resume-App) here. "  
 
 )
 
