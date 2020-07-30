be0eb21c063ee8764db72a1f13a2b6392ce2382a
alphadatagamma <alphadatagamma@users.noreply.github.com>
2020-07-29 23:04:06 -0400

    Firts commit

--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
 mkdir -p ~/.streamlit/
-echo “\
-[general]\n\
-email = \”abhishek.2.gupta@uconn.edu\”\n\
-“ > ~/.streamlit/credentials.toml
+
 echo “\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+\n\
 “ > ~/.streamlit/config.toml
